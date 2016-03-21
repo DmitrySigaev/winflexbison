551b75f112b3ac89a0ffd6e42972f8a06ad462e6
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-03-21 13:17:13 +0300

    clean

--- chocolatey/tools/chocolateyInstall.ps1
+++
@@ -1,2 +0,0 @@
-# download and unpack a zip file
-Install-ChocolateyZipPackage "winflexbison" "http://downloads.sourceforge.net/project/winflexbison/win_flex_bison-2.4.1.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
